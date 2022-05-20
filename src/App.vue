da62298684b03b9994d1cde78839a9160eaab4ee
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-05-20 10:08:59 +0100

    Aggiunti elementi di base progetto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  <HeaderComponent />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponent,
+  },
+
+  data(){
+
+  },
+
+  methods(){
+
+  },
+
+  mounted:{
+
+  },
+
+  computed:{
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@import "./assets/style/var";
+@import "./assets/style/global";
+
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+
+<header>
+  Prova
+</header>
+  
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent.vue"
+}
+</script>
+
+<style>
+
+</style>
