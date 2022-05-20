1ba913a3ee69ec4afa92898a966e6fa01ca0f6f5
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-05-20 10:49:20 +0100

    Creata interfaccia heade

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,21 @@
 <template>
 
 <header>
-Prova
+  <div class="img-container">
+    <img src="../assets/img/netflix-logo.png" alt="">
+  </div>
+
+  <div>
+    <select>
+      <option value="">Serie Tv</option>
+      <option value="">Film</option>
+    </select>
+  </div>
+
+  <div>
+    <input type="text" placeholder="Search">
+  </div>
+
 </header>
   
 </template>
@@ -12,8 +26,49 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+@import "../assets/style/var";
+
+header{
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 20px;
+  background-color: $secondary-color;
+
+  .img-container{
+    width: 200px;
+
+    img{
+      width: 100%;
+    }
+  }
+
+  select{
+    border: none;
+    padding: 10px;
+    border-radius: 5px;
+    background-color: $secondary-color;
+    color: white;
+    font-size: 16px
+  }
+
+  input{
+    border: none;
+    padding: 5px;
+    background-color: $secondary-color;
+    color: white;
+    border-bottom: 1px solid $primary-color;
+    
 
 
+    &:focus{
+      outline: none;
+    } 
+  }
+  
+}
 
 </style>
